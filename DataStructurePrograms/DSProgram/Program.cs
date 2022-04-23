7da38c31dec1bd76072865f50d7ba4024ed416ca
ANMOL SINGH RAJPUT <80009167+rajput8969@users.noreply.github.com>
2022-04-23 13:50:39 +0530

    Adding Data Structure

---
+++ DataStructurePrograms/DSProgram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DSProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Programs!");
+            Console.WriteLine("1.Unordered List");
+            Console.WriteLine("2.ordered List");
+            Console.WriteLine("3.Balanced Paranthesis");
+            Console.WriteLine("4.Banking Cash Counter");
+            Console.WriteLine("5.Palindrome Checker");
+            Console.WriteLine("6.print calender");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+                case 1:
+                    UnorderedList.Unordered();
+                    break;
+                case 2:
+                    OrderedList.Ordered();
+                    break;
+                case 3:
+                    char[] expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+                    BalancingParantheses.StackOperations stackOperations = new BalancingParantheses.StackOperations();
+                    bool result = stackOperations.Isbalanced(expression);
+                    if (result)
+                    {
+                        Console.WriteLine("Parantheses Balanced");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parantheses not Balanced");
+                    }
+                    break;
+                case 4:
+                    BankingCashCounter banking = new BankingCashCounter();
+                    banking.MaintainQueue();
+                    break;
+                case 5:
+
+                    PalindromeChecker<char> checker = new PalindromeChecker<char>();
+                    checker.IsPalindrome();
+                    break;
+                case 6:
+                    Calender.PrintCalender();
+                    break;
+
+            }
+
+        }
+    }
+}
